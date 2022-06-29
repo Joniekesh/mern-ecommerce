3623267cdfd384a052ea74993215c03267db416e
Okoro John <okorojohndarlington@gmail.com>
2022-06-29 17:24:34 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,13 @@ app.get("/api/config/paypal", (req, res) => {
 	res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
+const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 	});
 }
 
