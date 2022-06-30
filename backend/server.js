7c1eb308daf1d8c951ff5d7a8c638278409e7964
Okoro John <okorojohndarlington@gmail.com>
2022-06-30 10:54:07 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/../frontend/build")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, +"/../frontend/build/index.html"));
+		res.sendFile(path.resolve(__dirname, "/../frontend/build/index.html"));
 	});
 }
 
