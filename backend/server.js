592fe9653313c5e0da607b98e165d374e4e2cfec
Okoro John Darlington <okorojohndarlington@gmail.com>
2024-03-01 22:20:25 +0100

    Commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000" }));
+
 app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
@@ -24,7 +25,7 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () =>
   console.log(`SERVER runnning in ${process.env.NODE_ENV} MODE on PORT ${PORT}`)
 );
--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
-
-  // baseURL: "https://mernecommerceapp2.herokuapp.com/api",
+  baseURL: "https://mern-ecommerce-cswv.onrender.com/api",
 });
