07eab0c9cea4d47e86c57f14136e6671f63b9942
Okoro John Darlington <okorojohndarlington@gmail.com>
2024-03-25 20:03:20 +0100

    Commit

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -2,5 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://mern-ecommerce-cswv.onrender.com/api",
+  // baseURL: "https://mern-ecommerce-cswv.onrender.com/api",
 });
+
+// baseURL: "https://mern-ecommerce-cswv.onrender.com/api",
